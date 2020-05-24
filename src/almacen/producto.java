2cefc0108fcb8b576c601c3c647227103cd0e53f
yaguetskr <cabelloyago@gmail.com>
2020-05-24 21:02:54 +0200

    producto

---
+++ src/almacen/producto.java
@@ -0,0 +1,85 @@
+package almacen;
+
+public class producto {
+    
+    int Codigo;
+    String Nombre;
+    String descripcion;
+    int stock;
+    localizacion Localiza;
+    boolean pendientes;
+    
+    public void set_Codigo(int p)
+    {
+        Codigo=p;
+    }
+    public int get_Codigo()
+    {
+        return Codigo;
+        
+    }
+    public void set_Nombre(String p)
+    {
+        Nombre=p;
+    }
+    public String get_Nombre()
+    {
+        return Nombre;
+        
+    }
+    public void set_descripcion(String p)
+    {
+        descripcion=p;
+    }
+    public String get_descripcion()
+    {
+        return descripcion;
+        
+    }
+    public void set_stock(int p)
+    {
+        stock=p;
+    }
+    public int get_stock()
+    {
+        return stock;
+        
+    }
+    public void set_Localiza(localizacion p)
+    {
+        Localiza = p;
+    }
+    public localizacion get_Localiza()
+    {
+        return Localiza;
+        
+    }
+    public void set_pendientes(boolean p)
+    {
+        pendientes = p;
+    }
+    public boolean get_pendientes()
+    {
+        return pendientes;
+        
+    }
+    public producto(int cod, String nom,String desc,int stk,localizacion loc,boolean pend)
+    {
+        Codigo=cod;
+        Nombre =nom;
+        descripcion=desc;
+        stock = stk;
+        Localiza = loc;
+        pendientes=pend;
+    }
+    public producto()
+    {
+        Codigo=0;
+        Nombre ="a";
+        descripcion="a";
+        stock = 0;
+        Localiza = new localizacion();
+        pendientes=false;
+    }
+    
+}
