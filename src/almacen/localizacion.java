4eaa6bd0f592a73019c7ba428435fae662018c45
yaguetskr <cabelloyago@gmail.com>
2020-05-24 20:57:35 +0200

    localizacion

---
+++ src/almacen/localizacion.java
@@ -0,0 +1,51 @@
+package almacen;
+
+public class localizacion {
+
+	String pasillo;
+	String estanteria;
+	String estante;
+	
+	public void set_pasillo(String p) 
+	{
+		pasillo=p;
+	}
+	public String get_pasillo() 
+	{
+	return pasillo;	
+		
+	}
+	public void set_estanteria(String p) 
+	{
+		estanteria=p;
+	}
+	public String get_estanteria() 
+	{
+	return estanteria;	
+		
+	}
+	public void set_estante(String p) 
+	{
+		estante=p;
+	}
+	public String get_estante() 
+	{
+	return estante;	
+		
+	}
+	
+	public localizacion(String p, String es,String est)
+	{
+		pasillo = p;
+		estanteria = es;
+		estante = est;
+		
+	}
+	public localizacion()
+	{
+		pasillo = "0";
+		estanteria = "0";
+		estante = "0";
+		
+	}
+}
